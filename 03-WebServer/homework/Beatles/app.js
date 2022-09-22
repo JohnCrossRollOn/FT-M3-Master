3cdf6fdeeaa34db3d90659b9efc512377e0bae91
JohnCrossRollOn <juanrolon54@outlook.com>
2022-09-22 01:50:55 -0300

    made my first web server. Pretty legit

--- 03-WebServer/homework/Beatles/app.js
+++ 03-WebServer/homework/Beatles/app.js
@@ -1,24 +1,53 @@
-var http = require('http');
-var fs   = require('fs');
+const http = require('http');
+const fs = require('fs');
 
-var beatles=[{
-  name: "John Lennon",
-  birthdate: "09/10/1940",
-  profilePic:"https://blogs.correiobraziliense.com.br/trilhasonora/wp-content/uploads/sites/39/2020/10/CBNFOT081020100047-550x549.jpg"
-},
-{
-  name: "Paul McCartney",
-  birthdate: "18/06/1942",
-  profilePic:"http://gazettereview.com/wp-content/uploads/2016/06/paul-mccartney.jpg"
-},
-{
-  name: "George Harrison",
-  birthdate: "25/02/1946",
-  profilePic:"https://canaldosbeatles.files.wordpress.com/2012/02/george-george-harrison-8321345-438-600.jpg"
-},
-{
-  name: "Richard Starkey",
-  birthdate: "07/08/1940",
-  profilePic:"http://cp91279.biography.com/BIO_Bio-Shorts_0_Ringo-Starr_SF_HD_768x432-16x9.jpg"
+const beatles={
+  '/John%20Lennon': {
+    name: "John Lennon",
+    birthdate: "09/10/1940",
+    profilePic:"https://blogs.correiobraziliense.com.br/trilhasonora/wp-content/uploads/sites/39/2020/10/CBNFOT081020100047-550x549.jpg"
+  },
+  '/Paul%20McCartney':{
+    name: "Paul McCartney",
+    birthdate: "18/06/1942",
+    profilePic:"http://gazettereview.com/wp-content/uploads/2016/06/paul-mccartney.jpg"
+  },
+  '/George%20Harrison':{
+    name: "George Harrison",
+    birthdate: "25/02/1946",
+    profilePic:"https://canaldosbeatles.files.wordpress.com/2012/02/george-george-harrison-8321345-438-600.jpg"
+  },
+  '/Richard%20Starkey':{
+    name: "Richard Starkey",
+    birthdate: "07/08/1940",
+    profilePic:"http://cp91279.biography.com/BIO_Bio-Shorts_0_Ringo-Starr_SF_HD_768x432-16x9.jpg"
+  }
 }
-]
+
+http.createServer((req, res)=>{
+  if (req.url.includes('/api/')) {
+    var apiReqParams = '/'+req.url.split('/').slice(2).join('/');
+  };
+  if (beatles[req.url]) {
+    let {name, birthdate, profilePic} = beatles[req.url];
+    res.writeHead(200, {'Content-Type': 'text/html'});
+		let html = fs.readFileSync('./index.html', 'utf-8')
+		html = html
+    .replace('{name}', name)
+    .replace('{birthdate}', birthdate)
+    .replace('{profilePic}', profilePic);
+		res.end(html);
+  }
+  else if (req.url === '/api') {
+    res.writeHead(200, {'Content-Type': 'application/json'});
+		res.end(JSON.stringify(beatles));
+  }
+  else if(beatles[apiReqParams]){
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    res.end(JSON.stringify(beatles[apiReqParams]));
+  }
+  else {
+    res.writeHead(404, {'Content-Type': 'text/plain'});
+    res.end('404 Beatle Not Found');
+  }
+}).listen(3001, 'localhost')
--- 03-WebServer/homework/showByName/app.js
+++ 03-WebServer/homework/showByName/app.js
@@ -2,3 +2,20 @@ var fs  = require("fs")
 var http  = require("http")
 
 // Escribí acá tu servidor
+const imagenes = {
+    "/arcoiris_doge.jpg": "./images/arcoiris_doge.jpg",
+    "/badboy_doge.jpg": "./images/badboy_doge.jpg",
+    "/code_doge.jpg": "./images/code_doge.jpg",
+    "/resaca_doge.jpg": "./images/resaca_doge.jpg",
+    "/retrato_doge.jpg": "./images/retrato_doge.jpg",
+    "/sexy_doge.jpg": "./images/sexy_doge.jpg"
+}
+http.createServer((req, res)=>{
+    if(imagenes[req.url]) {
+        let doge = fs.readFileSync(imagenes[req.url]);
+        res.writeHead(200, {'Content-Type': 'image/jpg'});
+        res.end(doge)
+    } else {
+    res.writeHead(404, {'Content-Type': 'text/plain'});
+    res.end("404 Doge not found")}
+}).listen(3001, 'localhost');
